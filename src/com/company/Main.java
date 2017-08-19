791acf316e9a07b61fcf868b57ae8db3b0b42ed9
pablobello <77soft67@gmail.com>
2017-08-19 18:43:20 -0300

    commit de prueba en branch fruta

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,18 @@
 package com.company;
 
+import java.util.HashMap;
+
 public class Main {
+
+    public static HashMap<Integer, Integer> hash= new HashMap();
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("asd");
+        System.out.println("asd");
+
+        hash.put(1,1);
+
+        System.out.print(hash.keySet().toArray()[0]);
+
     }
 }
